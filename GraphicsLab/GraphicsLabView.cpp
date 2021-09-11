1bb529ee496a5d70d6092c36a91fe03b9ee268e2
zapto123 <tsoorckanukris@gmail.com>
2021-09-11 13:20:13 +0300

    implemented range specification

--- GraphicsLab/GraphicsLabView.cpp
+++ GraphicsLab/GraphicsLabView.cpp
@@ -33,14 +33,26 @@ BEGIN_MESSAGE_MAP(CGraphicsLabView, CView)
 	ON_WM_RBUTTONUP()
 END_MESSAGE_MAP()
 
-// CGraphicsLabView construction/destruction
 
 int factorialLength;
 uint64_t* factorialCache;
 
+const double epsilon = 0.01;
+const int gridLineLength = 2;
+const bool useFixedLimits = true;
+
+const int gridInterval = 25;
+
+const double a = -3;
+const double b = 5;
+const double step = 0.1;
+
+
 double CustomSinX(double x, double eps);
 double OtherCustomSinX(double x, double eps);
 uint64_t CachedFactorial(int k);
+
+// CGraphicsLabView construction/destruction
 
 CGraphicsLabView::CGraphicsLabView() noexcept
 {
@@ -67,6 +79,17 @@ BOOL CGraphicsLabView::PreCreateWindow(CREATESTRUCT& cs)
 
 // CGraphicsLabView drawing
 
+int NumToCoordX(double x, double a, double b, int width) {
+	const int padding = 5;
+	return padding + (x - a) / (b - a) * (width - 2 * padding);
+}
+
+int NumToCoordY(double y, double min, double max, int height) {
+	const int padding = 5;
+	return height - (padding + (y - min) / (max - min) * (height - 2 * padding));
+}
+
+
 void CGraphicsLabView::OnDraw(CDC* pDC)
 {
 	CGraphicsLabDoc* pDoc = GetDocument();
@@ -74,49 +97,171 @@ void CGraphicsLabView::OnDraw(CDC* pDC)
 	if (!pDoc)
 		return;
 
-	CRect rcClient;
-	GetClientRect(rcClient);
-	auto center = rcClient.CenterPoint();
+	if (useFixedLimits) {
+		// get all values
+		int valueCount = (int)((b - a) / step) + 1;
+		double *customResults = new double[valueCount];
+		double* baseResults = new double[valueCount];
 
-	pDC->MoveTo(0, center.y);
-	pDC->LineTo(rcClient.Width(), center.y);
+		double maxY;
+		double minY;
 
-	pDC->MoveTo(center.x, 0);
-	pDC->LineTo(center.x, rcClient.Height());
 
-	int gridInterval = 25;
-	int gridLength = 2;
+		for (int i = 0; i < valueCount; i++) {
+			double x = a + i * step;
+			customResults[i] = OtherCustomSinX(x, epsilon);
+			baseResults[i] = sin(x) / x;
 
-	int xGridLines = center.x / 25;
+			double tempMax = max(customResults[i], baseResults[i]);
+			double tempMin = min(customResults[i], baseResults[i]);
 
-	for (int i = 1; i <= xGridLines; i++) {
-		pDC->MoveTo(center.x + gridInterval * i, center.y - gridLength);
-		pDC->LineTo(center.x + gridInterval * i, center.y + gridLength + 1);
+			if (i == 0) {
+				maxY = tempMax;
+				minY = tempMin;
+			}
+			else 
+			{
+				maxY = max(tempMax, maxY);
+				minY = min(tempMin, minY);
+			}
+		}
 
-		pDC->MoveTo(center.x - gridInterval * i, center.y - gridLength);
-		pDC->LineTo(center.x - gridInterval * i, center.y + gridLength + 1);
+		CRect rcClient;
+		GetClientRect(rcClient);
+		auto center = rcClient.CenterPoint();
+		auto width = rcClient.Width();
+		auto height = rcClient.Height();
+
+		if (maxY * minY < 0) {
+			pDC->MoveTo(0,                NumToCoordY(0, minY, maxY, height));
+			pDC->LineTo(rcClient.Width(), NumToCoordY(0, minY, maxY, height));
+		}
+
+		if (a * b < 0) {
+			pDC->MoveTo(NumToCoordX(0, a, b, width), 0);
+			pDC->LineTo(NumToCoordX(0, a, b, width), rcClient.Height());
+		}
+
+		// getting floor then ceil to render one more line 
+		// most likely out of sight but does well for sin(x)/x
+		int firstGridLine = floor(a);
+		int lastGridLine = ceil(b);
+
+		for (int i = firstGridLine; i <= lastGridLine; i++) {
+			int xPos = NumToCoordX(i, a, b, width);
+			if (minY > 0) {
+				// draw grid at lower window limit
+				pDC->MoveTo(xPos, height - gridLineLength);
+				pDC->LineTo(xPos, height + 1);
+			}
+			else if (maxY < 0) {
+				// draw grid at upper window limit
+				pDC->MoveTo(xPos, 0);
+				pDC->LineTo(xPos, gridLineLength + 1);
+			}
+			else {
+				// draw as usual
+				pDC->MoveTo(xPos, NumToCoordY(0, minY, maxY, height) - gridLineLength);
+				pDC->LineTo(xPos, NumToCoordY(0, minY, maxY, height) + gridLineLength + 1);
+			}
+		}
+
+		firstGridLine = floor(minY);
+		lastGridLine = ceil(maxY);
+
+		for (int i = firstGridLine; i <= lastGridLine; i++) {
+			int yPos = NumToCoordY(i, minY, maxY, height);
+			if (a > 0) {
+				// draw grid at left window limit
+				pDC->MoveTo(0,					yPos);
+				pDC->LineTo(gridLineLength + 1, yPos);
+			}
+			else if (b < 0) {
+				// draw grid at right window limit
+				pDC->MoveTo(width - gridLineLength, yPos);
+				pDC->LineTo(width + 1,				yPos);
+			}
+			else {
+				// draw as usual
+				pDC->MoveTo(NumToCoordX(0, a, b, width) - gridLineLength,	  yPos);
+				pDC->LineTo(NumToCoordX(0, a, b, width) + gridLineLength + 1, yPos);
+			}
+		}
+
+		CPen redPen(PS_SOLID, 1, RGB(0, 255, 0));
+		pDC->SelectObject(&redPen);
+
+		double prevValue = customResults[0];
+		pDC->MoveTo(NumToCoordX(a, a, b, width), NumToCoordY(prevValue, minY, maxY, height));
+
+		for (int i = 0; i < valueCount; i++) {
+			pDC->LineTo(NumToCoordX(a + i * step, a, b, width), NumToCoordY(customResults[i], minY, maxY, height));
+			pDC->SetPixel(NumToCoordX(a + i * step, a, b, width), NumToCoordY(baseResults[i], minY, maxY, height), RGB(255, 0, 0));
+
+		}
+
+		/*
+		for (int i = 0; i < rcClient.Width(); i++) {
+			double x = (double)(i - center.x) / gridInterval;
+
+			if (x != 0) {
+				double y = OtherCustomSinX(x, epsilon);
+				pDC->LineTo(i, -(y * gridInterval) + center.y);
+
+				y = sin(x) / x;
+				pDC->SetPixel(i, -(y * gridInterval) + center.y, RGB(255, 0, 0));
+			}
+		}
+
+		*/
 	}
+	else {
+		CRect rcClient;
+		GetClientRect(rcClient);
+		auto center = rcClient.CenterPoint();
 
-	int yGridLines = center.y / 25;
+		pDC->MoveTo(0, center.y);
+		pDC->LineTo(rcClient.Width(), center.y);
 
-	for (int i = 1; i <= yGridLines; i++) {
-		pDC->MoveTo(center.x - gridLength, center.y + gridInterval * i);
-		pDC->LineTo(center.x + gridLength + 1, center.y + gridInterval * i);
+		pDC->MoveTo(center.x, 0);
+		pDC->LineTo(center.x, rcClient.Height());
 
-		pDC->MoveTo(center.x - gridLength, center.y - gridInterval * i);
-		pDC->LineTo(center.x + gridLength + 1, center.y - gridInterval * i);
-	}
+		int xGridLines = center.x / gridInterval;
 
-	for (int i = 0; i < rcClient.Width(); i++) {
-		double x = (double)(i - center.x) / gridInterval;
+		for (int i = 1; i <= xGridLines; i++) {
+			pDC->MoveTo(center.x + gridInterval * i, center.y - gridLineLength);
+			pDC->LineTo(center.x + gridInterval * i, center.y + gridLineLength + 1);
 
-		if (x != 0) {
-			double y = OtherCustomSinX(x, 0.01);
-			pDC->SetPixel(i, -(y * gridInterval) + center.y, RGB(255, 0, 0));
+			pDC->MoveTo(center.x - gridInterval * i, center.y - gridLineLength);
+			pDC->LineTo(center.x - gridInterval * i, center.y + gridLineLength + 1);
+		}
 
-			// TODO: change rendering method to use LineTo
-			y = sin(x) / x;
-			pDC->SetPixel(i, -(y * gridInterval) + center.y, RGB(0, 255, 0));
+		int yGridLines = center.y / gridInterval;
+
+		for (int i = 1; i <= yGridLines; i++) {
+			pDC->MoveTo(center.x - gridLineLength, center.y + gridInterval * i);
+			pDC->LineTo(center.x + gridLineLength + 1, center.y + gridInterval * i);
+
+			pDC->MoveTo(center.x - gridLineLength, center.y - gridInterval * i);
+			pDC->LineTo(center.x + gridLineLength + 1, center.y - gridInterval * i);
+		}
+
+		CPen redPen(PS_SOLID, 1, RGB(0, 255, 0));
+		pDC->SelectObject(&redPen);
+
+		double prevValue = OtherCustomSinX((double)center.x / gridInterval, 0.01);
+		pDC->MoveTo(0, -(prevValue * gridInterval) + center.y);
+
+		for (int i = 0; i < rcClient.Width(); i++) {
+			double x = (double)(i - center.x) / gridInterval;
+
+			if (x != 0) {
+				double y = OtherCustomSinX(x, epsilon);
+				pDC->LineTo(i, -(y * gridInterval) + center.y);
+
+				y = sin(x) / x;
+				pDC->SetPixel(i, -(y * gridInterval) + center.y, RGB(255, 0, 0));
+			}
 		}
 	}
 }
